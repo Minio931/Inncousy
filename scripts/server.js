d6356189e83b1908897336c74cf96e7b64004b90
Minio931 <34556717+Minio931@users.noreply.github.com>
2021-02-11 21:32:49 +0100

    Add files via upload

--- scripts/server.js
+++ scripts/server.js
@@ -1,28 +1,6 @@
-const http = require('https');
-
-const express = require('express');
-
-const app = express();
-
-
-app.use(express.static(`${__dirname}/../scripts`));
-
-
-const server = http.createServer(app);
-const socketio = require('socket.io').listen(server);
-const io = socketio(server);
-
-
-
-io.on('connection', (sock) =>{
-   sock.emit('message', 'You are connected');
-
-   sock.on('message', (text) => io.emit('message', text));
+const io = require('socket.io')();
+io.on('connection', client => {
+    client.emit('init', { data: 'hello world' })
 });
 
-server.on('error', (err) => {
-    console.error(err);
-})
-
-http.listen(5500, "127.0.0.1");
-server.listen(5500);
+io.listen(3000);
